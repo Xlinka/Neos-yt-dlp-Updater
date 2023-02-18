af1c227cbfceb581ac6b351845369e71a0982445
xlinka <Oliver.dunnington@outlook.com>
2023-02-18 03:58:11 +0000

    Comments

--- Neos yt-dlp Updater/Program.cs
+++ Neos yt-dlp Updater/Program.cs
@@ -8,17 +8,20 @@ namespace YtdlpUpdater
         static void Main(string[] args)
         {
             string customPath = Prompt("Enter a custom path for yt-dlp.exe (leave blank to skip): ");
+            // Set the default directories for NeosVR and Steam installations
             string neosDir = @"C:\Neos\app";
             string steamDir = @"C:\Program Files\Steam\steamapps\common\NeosVR";
             string[] targetDirs;
 
             if (!string.IsNullOrEmpty(customPath))
             {
+                // If a custom path was provided, update yt-dlp.exe at that path
                 Console.WriteLine($"Updating yt-dlp.exe at custom path: {customPath}");
                 UpdateYtdlp(customPath);
             }
             else
             {
+                // If no custom path was provided, prompt the user to choose which directories to update
                 string updateBoth = Prompt("Update yt-dlp.exe in both NeosVR and Steam directories? [Y/N]: ");
                 if (updateBoth.Equals("Y", StringComparison.OrdinalIgnoreCase))
                 {
@@ -45,6 +48,7 @@ namespace YtdlpUpdater
                     Console.WriteLine("Invalid input. Aborting update.");
                 }
             }
+            // Wait for user input before closing the console window
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
@@ -58,6 +62,7 @@ namespace YtdlpUpdater
 
         static void UpdateYtdlpAll(string[] targetDirs)
         {
+            // Update yt-dlp.exe in all directories specified in targetDirs
             foreach (string targetDir in targetDirs)
             {
                 if (File.Exists(Path.Combine(targetDir, "yt-dlp.exe")))
